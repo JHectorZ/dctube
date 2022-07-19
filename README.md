290b87bd6c3f5f0760611bf4b3f03f13cd88dd1f
JHectorZ <jesushellerque@gmail.com>
2022-07-19 16:47:02 -0600

    Upgrade, now you can download playlist!!

--- README.md
+++ README.md
@@ -6,16 +6,20 @@ you can get it with https://aur.archlinux.org/python-pytube.git
 
 This will be the structure to run:
 
-python index.py <link> <type> -q <quality (optional)> 
+python index.py <link> <type> -lv <Playlist: True o False (optional)> -q <quality (optional)> 
 
 example:
   python index.py https://www.youtube.com/watch?v=B9RgougnhiE video 
   
   python index.py https://www.youtube.com/watch?v=B9RgougnhiE song
   
+  python index.py https://www.youtube.com/watch?v=B9RgougnhiE song -lv True (if playlist) 
+
+  python index.py https://www.youtube.com/watch?v=B9RgougnhiE song -lv False (if not playlist) 
+
   python index.py https://www.youtube.com/watch?v=B9RgougnhiE video -q low
-  
- 
+
+
   
 This is provisional in what will be done in package for linux in future commits!!
   
--- index.py
+++ index.py
@@ -1,19 +1,17 @@
 import pytube 
+from pytube import Playlist
 import argparse
 import os
 
 
-#dctube link video -q high -p ~/download
-#dctube link song -p ~/download
-
-
 class dctube():
 
-    def __init__(self, link, type, quality = 'high'):
+    def __init__(self, link, type, listvideo = False, quality = 'high'):
     
         self.link = link
         self.type = type
         self.quality = quality
+        self.listvideo = listvideo
 
 
     def download_path(self):
@@ -25,19 +23,44 @@ class dctube():
     def download(self):
         
         if self.type == 'song':
-            pytube.YouTube(self.link).streams.get_audio_only().download(self.down_path)
+
+            if self.listvideo:
+                l_video = Playlist(self.link)
+
+                for video in l_video.videos:
+                    video.streams.get_audio_only().download(self.down_path)
+
+            else:
+                pytube.YouTube(self.link).streams.get_audio_only().download(self.down_path)
 
 
         elif self.type == 'video':
+            
+            if self.listvideo:
+                l_video = Playlist(self.link)
 
-            if self.quality == 'high' or self.quality == 'default':
-                pytube.YouTube(self.link).streams.get_highest_resolution().download(self.down_path)
+                if self.quality == 'high' or self.quality == 'default':
 
-            elif self.quality == 'low':
-                pytube.YouTube(self.link).streams.get_lowest_resolution().download(self.down_path)
+                    for video in l_video.videos:
+                        video.streams.get_highest_resolution().download(self.down_path)
+
+                elif self.quality == 'low':
+
+                    for video in l_video.videos:
+                        video.streams.get_lowest_resolution().download(self.down_path)
+
+                else:
+                    print('[-] Quality error')
 
             else:
-                print('[-] Quality error')
+                if self.quality == 'high' or self.quality == 'default':
+                    pytube.YouTube(self.link).streams.get_highest_resolution().download(self.down_path)
+
+                elif self.quality == 'low':
+                    pytube.YouTube(self.link).streams.get_lowest_resolution().download(self.down_path)
+
+                else:
+                    print('[-] Quality error')
 
         else:
             print('[-] Type error')
@@ -49,10 +72,11 @@ if __name__ == '__main__':
     aft = argparse.ArgumentParser()
     aft.add_argument('link', type = str, help = 'youtube link')
     aft.add_argument('type', type = str, help = 'song or video')
+    aft.add_argument('-lv', type = bool, help = 'is playlist?, default: no', default = False)
     aft.add_argument('-q', type = str, help = 'quality video, default: high', default = 'high')
     argu = aft.parse_args()
 
-    dctube_p = dctube(argu.link, argu.type, argu.q)
+    dctube_p = dctube(argu.link, argu.type, argu.lv, argu.q)
     dctube_p.download_path()
     dctube_p.download()
 
